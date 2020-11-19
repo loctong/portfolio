e5ac992726e9f9601628d8447c15feca87bee602
Tong Bao Loc <loc.tong@openoffice.com.au>
2020-11-19 10:36:33 +0700

    Updates

---
+++ precache-manifest.2b682d0098ab9cd7d7fa6f3758237b04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b56c5e9fb8f300c276c5",
+    "url": "/portfolio/static/css/main.e1b1599f.chunk.css"
+  },
+  {
+    "revision": "b56c5e9fb8f300c276c5",
+    "url": "/portfolio/static/js/main.b56c5e9f.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "61598648cc051fff795a86ec99f7410a",
+    "url": "/portfolio/index.html"
+  }
+];
